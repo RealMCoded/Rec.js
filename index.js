aa209ba12dd8f4cc0ce9e6186d7fe4310d9b3eea
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2023-01-01 23:30:07 -0500

    autostart versions if undefined

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const chalk = require("chalk")
 const chalkTable = require('chalk-table')
 const { version, dev } = require("./package.json")
-const ver = process.argv[2]
+const { defaultVersion } = require("./config.json")
+let ver = process.argv[2]
 const tryText = "Example Usage: \'npm run serve 2016\'."
 
 //Yellow Rec Room logo
@@ -35,7 +36,15 @@ if (dev.is_dev == true) console.log(`${chalk.red(`\n⚠️WARNING⚠️`)}\nThis
 This version contains unfinished and untested code that may result in crashes.
 Please report any bugs you may find to stuartt#5679!\n`)
 
-if (ver == undefined) {return console.error(`${chalk.red('ERROR:')} No version specified!.\n${tryText}`)}
+if (ver == undefined) {
+    console.error(`${chalk.yellow('WARN:')} No version specified! Defaulting to version set in config.json...`)
+    switch (defaultVersion) {
+        case "2016": {ver = "2016";} break;
+        case "2017": {ver = "2017";} break;
+        case "2018": {ver = "2018";} break;
+        default: {return console.error(`${chalk.red('ERROR:')} Invalid version specified in config.json. Expected "2016". Got "${defaultVersion}".`)} break;
+    }
+}
 
 switch(ver){
     case "2016": {
