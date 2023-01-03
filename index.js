0543a3b3bdd6300dc68182d1501791f5a6c210af
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2023-01-03 18:25:06 -0500

    Placeholder config command

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ let cmd = process.argv[2]
 const fs = require("node:fs")
 const tryText = "Example Usage: \'npm run serve 2016\'."
 const child_process = require('child_process')
-
-//Yellow Rec Room logo
 process.title = "RecNet.js"
+
+//Print logo
 console.log(`
 [49m [38;5;185;49m▄▄[48;5;185m                                     [38;5;185;49m▄▄[49m [m
 [38;5;185;49m▄[48;5;185m    [49;38;5;185m▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀[48;5;185m    [38;5;185;48;5;185m▄[m
@@ -32,6 +32,7 @@ console.log(`
 [48;5;185m   [49m                                     [48;5;185m   [m
 [49;38;5;185m▀[48;5;185m                                         [38;5;185;48;5;185m▄[m
 [49m [49;38;5;185m▀▀[48;5;185m                                     [49;38;5;185m▀▀[49m [m`)
+
 //other welcome code
 console.log(chalk.yellow(`\nRecNet.js Version ${version}${(dev.is_dev == true ? dev.suffix : "")} (commit ${child_process.execSync('git rev-parse HEAD').toString().substring(0, 7)})`))
 if (dev.is_dev == true) console.log(`${chalk.red(`\n⚠️WARNING⚠️`)}\nThis is a pre-release version of RecNet.js.
@@ -54,7 +55,8 @@ switch(cmd){
     case "serve": {
         require("./src/serve.js").run(process.argv[3])
     } break;
-    //case "2017": {console.error("(2017 soon)")} break;
-    //case "2018": {console.error("(2018 soon)")} break;
+    case "config": {
+        require("./src/config.js").run()
+    } break;
     default: {console.error(`${chalk.red('[ERROR]')} Invalid or no command specified.\n${tryText}`)} break;
 }
---
+++ src/config.js
@@ -0,0 +1,5 @@
+function run() {
+    console.log("placeholder message - not working yet!")
+}
+
+module.exports = { run }
