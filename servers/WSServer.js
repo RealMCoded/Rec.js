949dd529a16d4ee3881692cd499670f2909156ec
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2023-01-04 23:24:05 -0500

    WebSocket uses `ws`, not `express-ws`

--- servers/WSServer.js
+++ servers/WSServer.js
@@ -1,13 +1,5 @@
-/*
-* TODO: Look into a better Websocket implementation instead of express. maybe the "ws" package?
-*/
-
 const chalk = require('chalk')
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-const expressWs = require('express-ws')(app);
-app.use(morgan(`${chalk.blueBright("[WS]")} :method ":url" :status - :response-time ms`))
+const { WebSocketServer } = require('ws');
 
 let port;
 
@@ -21,26 +13,15 @@ function start(servePort = 20161){
 }
 
 function serve() {
-    //Test endpoint
-    app.ws('/echo', function(ws, req) {
-        ws.on('message', function(msg) {
-          ws.send(`[RecNet.js] ${msg}`);
-        });
-    })
-
-    app.ws('/api/notification/*', function(ws, req) {
-        //TODO: Get this to actually work.
-        ws.send(`[]`);
-    })
+    const wss = new WebSocketServer({ port: port });
     
-    app.ws('/hub/v1', function(ws, req) {
-        //TODO: Get this to actually work.
-        ws.send(`[]`);
-    })
-
-    app.listen(port, () => {
-        console.log(`${chalk.blueBright("[WS]")} WS started on port ${port}`)
-    })
+    wss.on('connection', function connection(ws) {
+        ws.on('message', function message(data) {
+            console.log(`${chalk.blueBright("[WS]")} received: ${data}`);
+            ws.send(JSON.stringify(data))
+        });
+    });
+    console.log(`${chalk.blueBright("[WS]")} WS started on port ${port}`)
 }
 
 module.exports = { start }
