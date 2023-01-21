d07bcf5c1fce506f3d42283f06fb9afa9f8424ec
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2023-01-21 11:37:06 -0500

    Fixed incorrect CDN port

--- shared/config.js
+++ shared/config.js
@@ -1,9 +1,10 @@
 const { version, dev } = require("../package.json")
+const { ports } = require("../config.json")
 
 function config(){
     return JSON.stringify({
         MessageOfTheDay: `Rec.js Version ${version}${(dev.is_dev == true ? dev.suffix : "")} (commit ${process.commit})`,
-        CdnBaseUri: "http://localhost:20182/",
+        CdnBaseUri: `http://localhost:${ports.IMG}/`,
         LevelProgressionMaps:[{"Level":0,"RequiredXp":1},{"Level":1,"RequiredXp":2},{"Level":2,"RequiredXp":3},{"Level":3,"RequiredXp":4},{"Level":4,"RequiredXp":5},{"Level":5,"RequiredXp":6},{"Level":6,"RequiredXp":7},{"Level":7,"RequiredXp":8},{"Level":8,"RequiredXp":9},{"Level":9,"RequiredXp":10},{"Level":10,"RequiredXp":11},{"Level":11,"RequiredXp":12},{"Level":12,"RequiredXp":13},{"Level":13,"RequiredXp":14},{"Level":14,"RequiredXp":15},{"Level":15,"RequiredXp":16},{"Level":16,"RequiredXp":17},{"Level":17,"RequiredXp":18},{"Level":18,"RequiredXp":19},{"Level":19,"RequiredXp":20},{"Level":20,"RequiredXp":21}],
         MatchmakingParams:{
             PreferFullRoomsFrequency: 1,
