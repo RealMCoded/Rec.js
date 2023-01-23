d2c9df79b01640b09b603b5dedab80fca1161d51
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2023-01-23 17:17:13 -0500

    Install and Run files

---
+++ Install.bat
@@ -0,0 +1,8 @@
+@echo off
+title Rec.js Install
+cd ..
+@echo on
+npm i
+@echo off
+echo Installed!
+pause
---
+++ Run.bat
@@ -0,0 +1,6 @@
+@echo off
+IF exist node_modules ( 
+	npm run rec.js
+) ELSE ( 
+	echo You did not install the requirements!
+)
