2ed7c3a23fc953dc3470f7c3803adb2103d374e4
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2023-02-10 14:40:29 -0500

    why didn't i use `=>` here [part 2]

--- shared/profile.js
+++ shared/profile.js
@@ -4,11 +4,11 @@ async function setPFP(req){
     //TODO: Figure out how to remove the header information from the request.
     var f=fs.createWriteStream(`./images/${Math.floor(Date.now() / 1000)}.png`);
 
-    req.on('data', function (chunk) {
+    req.on('data', (chunk) => {
         f.write(chunk);
     });
 
-    req.on('end',function(){
+    req.on('end', () => {
         f.end();
         return;
     });
